f9a69e0593fafb3883573517694b8be8d70a8e72
Jamaal Kirkparick <jkirkpatrick@q-centrix.com>
2024-01-04 22:13:38 -0800

    added additional setup

---
+++ hydra/modules/environment.py
@@ -0,0 +1,8 @@
+
+
+def validate_file_system():
+    """
+    Verify that this instance can access the file system for read and write
+    and that the root is empty
+    """
+    pass
---
+++ hydra/modules/versionctl.py
@@ -0,0 +1,11 @@
+
+
+def fetch_project_structure(project_type):
+    """
+    Return structure as a dictionary according to passed type
+
+    args:
+
+    project_type - str - type name of the selected project to build
+    """
+    pass

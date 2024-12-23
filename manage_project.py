ad793f3230ec4ff46d17ef59aa84fb2378b49ea3
Jamaal Kirkparick <jkirkpatrick@q-centrix.com>
2024-12-22 16:57:48 -0800

    integrated version control in build scripting. Bumped fresh devops branch version

--- manage_project.py
+++ manage_project.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+from packaging.version import Version as v
 import argparse
 import subprocess
+import requests
 
 
 parser = argparse.ArgumentParser(description="build the hydra project")
@@ -33,20 +35,32 @@ if args.tox:
         ]
     )
 else:
-    print("\nmanage_project: Running without Tox. Give --tox to run tests.")
+    print("\nmanage_project: Running without Tox. Use --tox to run tests.")
 
 if args.build:
-    print("Building hydra project...")
-    #TODO: fetch latest version and increment according to version bump
+    print("\nmanage_project: Building hydra project...")
 
-    subprocess.run(
-        [
-            "python",
-            "-m",
-            "build"
-        ]
-    )
+    if not args.version_bump:
+        print("\nmanage_project: Build aborted. A new version level is required. Use --version-bump\n")  # noqa
+    else:
+        #TODO: fetch latest version and increment according to version bump
+        response = requests.get(
+            'https://api.github.com/repos/kjamaal/hydra/releases/latest'
+        )
+        with open("hydra/__init__.py", "r") as mod:
+            this_version = mod.read().split("= ")[1].replace("\"", "")
+            if v(f"{this_version}") > v(response.json()["name"]):
+                print(f"build {this_version}")
+                    #subprocess.run(
+                    #    [
+                    #        "python",
+                    #        "-m",
+                    #        "build"
+                    #    ]
+                    #)
+            else:
+                print("\nmanage_project: Build aborted. Currently Released version is higher. You should rebase it in and try to build/test again.")  # noqa
 else:
     print("\nmanage_project: Running without Building. Give --build to build the hydra project")  # noqa
 
-    #TODO: cleanup previous builds
+    #TODO: cleanup previous build artifacts in dist/
